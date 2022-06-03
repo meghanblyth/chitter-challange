0be9d8d214636c88edf5082cbc91321554c10782
meghanblyth <megblyth@gmail.com>
2022-06-03 16:00:56 +0100

    Preparing for postman testing

--- server/server.js
+++ server/server.js
@@ -1,2 +1,15 @@
-console.log('Hello world!')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000 
+
+const app = express()
+
+app.get('/peeps', (req, res) => {
+  res.send('Get peeps')
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
+
+
 
