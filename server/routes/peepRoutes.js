b7c1dbc8640563838379e7abcdd51a6e867ef439
Dashiell Boyles <dashiellboyles@hotmail.com>
2022-06-07 16:11:57 +0100

    Added post put and delete to the routes

--- server/routes/peepRoutes.js
+++ server/routes/peepRoutes.js
@@ -5,4 +5,17 @@ router.get('/', (req, res) => {
   res.status(200).json({ message: 'Get peeps' })
 })
 
+router.post('/', (req, res) => {
+  res.status(200).json({ message: 'Post new peep' })
+})
+
+router.put('/:id', (req, res) => {
+  res.status(200).json({ message: `Update peep ${req.params.id}` })
+})
+
+router.delete('/:id', (req, res) => {
+  res.status(200).json({ message: `Update peep ${req.params.id}` })
+})
+
+
 module.exports = router 
