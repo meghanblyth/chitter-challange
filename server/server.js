455b502095da0ed7e72baf61a9e2728452219b08
meghanblyth <megblyth@gmail.com>
2022-06-07 16:06:33 +0100

    Setup of routes and first get request working

--- server/server.js
+++ server/server.js
@@ -4,9 +4,7 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
-app.get('/peeps', (req, res) => {
-  res.send('Get peeps')
-})
+app.use('/peeps', require('./routes/peepRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
 
