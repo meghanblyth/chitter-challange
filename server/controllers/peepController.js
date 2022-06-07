4db73cfc6f600ddf8b32e4c1dc0e5ff28608271d
meghanblyth <megblyth@gmail.com>
2022-06-07 17:55:17 +0100

    Adding async handler

--- server/controllers/peepController.js
+++ server/controllers/peepController.js
@@ -1,22 +1,24 @@
-const getPeeps = (req, res) => {
-  res.status(200).json({ message: 'Get peeps' })
-}
+const asyncHandler = require('express-asyncHandler')
 
-const postPeeps = (req, res) => {
+const getPeeps = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Get peeps' })
+})
+
+const postPeeps = asyncHandler(async (req, res) => {
   if(!req.body.text) {
     res.status(400)
     throw new Error('Please add a text field')
-  } 
+  }
   res.status(200).json({ message: 'Post new peep' })
-}
+})
 
-const updatePeeps = (req, res) => {
+const updatePeeps = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update peep ${req.params.id}` })
-}
+})
 
-const deletePeeps = (req, res) => {
+const deletePeeps = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete peep ${req.params.id}` })
-}
+})
 
 module.exports = {
   getPeeps, 
