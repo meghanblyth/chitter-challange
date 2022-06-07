de0c9357a8524919a5ab050199e8c71c4c8f4fb5
meghanblyth <megblyth@gmail.com>
2022-06-07 17:45:40 +0100

    Adding middleware set up

--- server/controllers/peepController.js
+++ server/controllers/peepController.js
@@ -3,6 +3,10 @@ const getPeeps = (req, res) => {
 }
 
 const postPeeps = (req, res) => {
+  if(!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field')
+  } 
   res.status(200).json({ message: 'Post new peep' })
 }
 
