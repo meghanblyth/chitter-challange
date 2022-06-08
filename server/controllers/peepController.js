8015ef3d64303378b837b05ddd2e6be2b4db1554
meghanblyth <megblyth@gmail.com>
2022-06-08 16:40:48 +0100

    Post functionality

--- server/controllers/peepController.js
+++ server/controllers/peepController.js
@@ -1,15 +1,25 @@
 const asyncHandler = require('express-async-handler')
 
+const Peep = require('../models/peepModel')
+
 const getPeeps = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get peeps' })
+  const peeps = await Peep.find()
+
+  res.status(200).json(peeps)
 })
 
 const postPeeps = asyncHandler(async (req, res) => {
+
   if(!req.body.text) {
     res.status(400)
     throw new Error('Please add a text field')
   }
-  res.status(200).json({ message: 'Post new peep' })
+
+  const peeps = await Peep.create({
+    text: req.body.text
+  })
+
+  res.status(200).json(peeps)
 })
 
 const updatePeeps = asyncHandler(async (req, res) => {
