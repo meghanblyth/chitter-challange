1f9eec96efb49b7c53f0d6d19687497eb6efef29
Dashiell Boyles <dashiellboyles@hotmail.com>
2022-06-13 18:14:06 +0100

    Able to add a new Peep

--- client/src/components/ListPeeps.js
+++ client/src/components/ListPeeps.js
@@ -4,6 +4,18 @@ const ListPeeps = () => {
   
   const [peeps, setPeeps] = useState([])
 
+  // const deletePeeps = async (id) => {
+  //   try {
+  //     const binPeep = await fetch(`http://localhost:5000/peeps/${id}`, {
+  //       method: "DELETE"
+  //     })
+  //     //console.log(binPeep)
+  //   setPeeps(peeps.filter(peep => peep.peeps_id !== id))
+  //   } catch (err) {
+  //     console.error(err.message)
+  //   }
+  // }
+
   const getPeeps = async () => {
     try { 
       const response = await fetch("http://localhost:5000/peeps") 
@@ -13,11 +25,11 @@ const ListPeeps = () => {
       console.error(err.message)
     }
   }
+
   useEffect(() => {
     getPeeps();
   }, []);
 
-  
   return (
     <Fragment> 
       <table className="table mt-5 text-centre">
@@ -30,7 +42,7 @@ const ListPeeps = () => {
         </thead>
         <tbody>
           {peeps.map(peep => (
-            <tr key={peep.peeps_id}>
+            <tr key={peep._id}>
               <td>{peep.text}</td>
               <td>Edit</td>
               <td>Delete</td>
--- client/src/components/PostPeeps.js
+++ client/src/components/PostPeeps.js
@@ -4,27 +4,46 @@ const PostPeeps = () => {
   const [peep, setPeep] = useState("")  
   
   
-  const onSubmitForm = async e => {
-    //below so it doesnt refresh
-    e.preventDefault();
-    try {
+  // const onSubmitForm = async e => {
+  //   //below so it doesnt refresh
+  //   e.preventDefault();
+  //   try {
       
-      const body = { peep };
-      const response = await fetch("http://localhost:5000/peeps", {
+  //     const body = { peep };
+  //     const response = await fetch("http://localhost:5000/peeps", {
+  //       method: "POST",
+  //       headers: {"Content-Type": "application/json"},
+  //       body: JSON.stringify(body)
+  //     })
+  //     console.log(response)
+  //     //window.location = "/";
+  //   } catch (err) {
+  //     console.error(err.message)
+  //   }
+  // };
+
+  const NewPeep = async () => {
+    await fetch("http://localhost:5000/peeps", {
         method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(body)
-      })
-      window.location = "/";
-    } catch (err) {
-      console.error(err.message)
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+                text: peep
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+
+
+            setPeep(data);
+            
+        })
+        .catch(err => console.log(err))
     }
-  };
 
   return (
     <Fragment>
     <h1 className='text-center mt-5'>Post a NEW PEEPPPPPPP</h1>
-    <form className='d-flex mt-5' onSubmit={onSubmitForm}>
+    <form className='d-flex mt-5' onSubmit={NewPeep}>
       <input 
         type='text' 
         className='form-control' 
